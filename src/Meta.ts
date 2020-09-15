b48e98696207eddc74cd06c34363a71b7bc8494c
Wietse Wind <mail@wietse.com>
2020-09-15 20:35:02 +0200

    Import types @ Meta instead of locally defined

--- src/Meta.ts
+++ src/Meta.ts
@@ -4,53 +4,14 @@ import {hostname} from 'os'
 import packageJson from '../package.json'
 import {throwIfError} from './utils'
 
+import type {
+  ApplicationDetails,
+  Pong,
+  AnyJson,
+  CuratedAssetsResponse
+} from './types'
+
 const log = Debug('xumm-sdk:main')
-
-export interface ApplicationDetails {
-  quota: object
-  application: {
-    uuidv4: string
-    name: string
-    webhookurl: string
-    disabled: number
-  },
-  call: {
-    uuidv4: string
-  }
-}
-
-export interface Pong {
-  pong: boolean
-  auth: ApplicationDetails
-}
-
-export interface AnyJson {
-  [key: string]: AnyJson | any
-}
-
-export interface CuratedAssetsResponse {
-  issuers: string[]
-  currencies: string[]
-  details: {
-    [issuer: string]: {
-      id: number
-      name: string
-      domain?: string
-      avatar?: string
-      shortlist: number
-      currencies: {
-        [currencyCode: string]: {
-          id: number
-          issuer_id: number
-          currency: string
-          name: string
-          avatar?: string
-          shortlist: number
-        }
-      }
-    }
-  }
-}
 
 export class Meta {
   private apiKey: string
