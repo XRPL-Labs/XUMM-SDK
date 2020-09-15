dc8a738dc9c59af2b792c9a0ee1bf0d0b2d8f35f
Wietse Wind <mail@wietse.com>
2020-09-15 12:26:53 +0200

    Validate UUID format, prepare fetch for mocks

--- src/Meta.ts
+++ src/Meta.ts
@@ -1,7 +1,8 @@
 import Debug from 'debug'
-import Fetch from 'node-fetch'
+import fetch from 'node-fetch'
 import {hostname} from 'os'
 import packageJson from '../package.json'
+import {throwIfError} from './utils'
 
 const log = Debug('xumm-sdk:main')
 
@@ -58,6 +59,12 @@ export class Meta {
   constructor (apiKey: string, apiSecret: string) {
     log('Constructed')
 
+    const uuidRe = new RegExp('^[a-f0-9]{8}\-[a-f0-9]{4}\-[a-f0-9]{4}\-[a-f0-9]{4}\-[a-f0-9]{12}$')
+
+    if (!uuidRe.test(apiKey) || !uuidRe.test(apiSecret)) {
+      throw new Error('Invalid API Key and/or API Secret. Use dotenv or constructor params.')
+    }
+
     this.apiKey = apiKey
     this.apiSecret = apiSecret
 
@@ -85,7 +92,7 @@ export class Meta {
         'x-api-secret': this.apiSecret
       }
 
-      const request = await Fetch('https://xumm.app/api/v1/platform/' + endpoint, {
+      const request = await fetch('https://xumm.app/api/v1/platform/' + endpoint, {
         method,
         body,
         headers
@@ -102,7 +109,14 @@ export class Meta {
 
   public async ping (): Promise<ApplicationDetails> {
     const pong = await this.call<Pong>('ping')
-    return pong.auth
+
+    throwIfError(pong)
+
+    if (typeof pong.auth !== 'undefined') {
+      return pong.auth
+    }
+
+    throw new Error(`Unexpected response for ping request`)
   }
 
   async getCuratedAssets (): Promise<CuratedAssetsResponse> {
