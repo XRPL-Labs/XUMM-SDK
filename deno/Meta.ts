2d1ca65bd67b5a39107071222eca23d18673eb41
Wietse Wind <mail@wietse.com>
2020-12-23 06:37:03 +0100

    Deno 0.2.5

--- deno/Meta.ts
+++ deno/Meta.ts
@@ -47,7 +47,7 @@ export class Meta {
 
       const headers = {
         'Content-Type': 'application/json',
-        'User-Agent': 'xumm-sdk/deno:0.2.4',
+        'User-Agent': 'xumm-sdk/deno:0.2.5',
         'x-api-key': this.apiKey,
         'x-api-secret': this.apiSecret
       }
