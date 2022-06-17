dba6bea136880467314b306c224012fe664388ad
Wietse Wind <mail@wietse.com>
2022-06-17 16:09:45 +0200

    1.3.4, types

--- deno/types/xumm-api/index.ts
+++ deno/types/xumm-api/index.ts
@@ -153,6 +153,9 @@ export interface XummGetPayloadResponse {
     dispatched_nodetype: string | null
     dispatched_to: string | null
     dispatched_result: string | null
+    dispatched_to_node: boolean | null
+    environment_nodeuri: string | null
+    environment_nodetype: string | null
     multisign_account: string | null
     account: string | null
     signer: string | null
