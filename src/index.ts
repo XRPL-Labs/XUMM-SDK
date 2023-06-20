b1a88d7d13b97505b867ebc208531a5de7183997
Wietse Wind <mail@wietse.com>
2023-06-20 13:14:31 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import {Push} from './Push'
 import {JwtUserdata} from './JwtUserdata'
 import type * as XummTypes from './types/xumm-api'
 import type * as SdkTypes from './types/index'
-import type { xAppOttData, UserTokenValidity, xAppJwtOtt } from './types/index'
-import { Buffer } from 'buffer/'
+import type {xAppOttData, UserTokenValidity, xAppJwtOtt} from './types/index'
+import {Buffer} from 'buffer/'
 
 const log = Debug('xumm-sdk')
 
@@ -150,7 +150,8 @@ class XummSdkJwt extends XummSdk {
               const localStorageJwtData = window?.localStorage?.['XummSdkJwt']?.split(':')
               const localStorageJwt = JSON.parse(localStorageJwtData?.slice(1)?.join(':'))
               if (localStorageJwt?.jwt) {
-                const jwtContents = JSON.parse(Buffer.from(localStorageJwt.jwt.split('.')?.[1], 'base64').toString('utf8'))
+                const decodedJwt = Buffer.from(localStorageJwt.jwt.split('.')?.[1], 'base64').toString('utf8')
+                const jwtContents = JSON.parse(decodedJwt)
                 if (jwtContents?.exp) {
                   const validForSec = jwtContents?.exp - Math.floor((new Date()).getTime() / 1000)
                   console.log('Restoring OTT ' + localStorageJwtData?.[0])
